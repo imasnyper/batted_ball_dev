3811797e02cb627ef8f885f5527aed5ceadf7e74
Daniel Hayes <danihaye@gmail.com>
2019-08-23 14:10:03 -0400

    almost there

--- frontend/src/views/batted_balls.tsx
+++ frontend/src/views/batted_balls.tsx
@@ -9,17 +9,17 @@ export default function BattedBalls(props: any) {
     if(loading) return <p>Loading...</p>
     if(error) return <p>Error :(</p>
 
-    console.log(data.allBattedBalls.edges)
+    console.log(data.allBattedBalls.edges[0])
     // @ts-ignore
     // eslint-disable-next-line array-callback-return
-    return data.allBattedBalls.edges.map(({id, pitchername, battername, pitchSpeed, launchSpeed}) => {
+    return data.allBattedBalls.edges.map(({node}) => {
         // eslint-disable-next-line no-unused-expressions
-        <div key={id}>
+        <div key={node.id}>
             <p>
-                Pitcher: {pitchername}
-                Batter: {battername}
-                Pitch Speed: {pitchSpeed}
-                Launch Speed: {launchSpeed}
+                Pitcher: {node.pitchername}
+                Batter: {node.battername}
+                Pitch Speed: {node.pitchSpeed}
+                Launch Speed: {node.launchSpeed}
             </p>
         </div>
     })
